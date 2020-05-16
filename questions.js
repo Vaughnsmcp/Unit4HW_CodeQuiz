0d7584dd40b71ae45ec0a361788ee0a8e85c87df
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-05-15 17:52:56 -0700

    questions

---
+++ questions.js
@@ -0,0 +1,51 @@
+const questions = [{
+        title: "WHAT... is your name?",
+        choices: [
+            "Brave Sir Robin",
+            "Thelonious",
+            "Sir Lancelot",
+            "Steve",
+        ],
+        answer: "Sir Lancelot",
+    },
+    {
+        title: "WHAT... is your quest?",
+        choices: [
+            "I'm off to see the wizzard",
+            "To seek the Holy Grail",
+            "Nitzche said what's the point anyway",
+            "Oprah, 'cuz I need a new car",
+
+        ],
+        answer: "To seek the Holy Grail"
+    },
+    {
+        title: "WHAT... is your favorite color?",
+        choices: [
+            "Blue",
+            "Green",
+            "Blue... NO! GREEN!",
+        ],
+        answer: "Blue",
+    },
+    {
+        title: "WHAT... is the capital of Assyria?",
+        choices: [
+            "WHAT? I don't know that!",
+            "Where is Assyria?",
+            "Assur",
+            "Blue... No! GREEN!"
+        ],
+        answer: "Assur"
+    },
+    {
+        title: "WHAT... is the airspeed velocity of an unladen swallow?",
+        choices: [
+            "11 meters/second.",
+            "24 MPH",
+            "African or European?",
+            "All of the above"
+        ],
+        answer: "All of the above"
+    }
+];
